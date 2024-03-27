5ada5d8ba59a38c23d743e024cbb47fb0a0059b1
beerux <bastian_daniel@hotmail.com>
2024-03-27 12:37:39 +0100

    css changed .btn scale

--- 50Projects50Days/Day2-ProgressSteps/script.js
+++ 50Projects50Days/Day2-ProgressSteps/script.js
@@ -1,14 +1,17 @@
-const progress = document.getElementById('progress');
-const previous = document.getElementById('prev');
-const next = document.getElementById('next');
+const progress = document.getElementById('progress')
+const previous = document.getElementById('previous')
+const next = document.getElementById('next')
 
 //Use "queryselectorAll" since there's more than 1
-const circleNodes = document.querySelectorAll('.circle');
+const circleNodes = document.querySelectorAll('.circle')
+// console.log(circleNodes)
 
 //Highlights active circlenode
-let currentActive = 1;
+let currentActive = 1
+console.log(currentActive)
 
 //Listen for a click and add 1 to any element with "active" class
-next.addEventListener('click', () => {
-     currentActive++ 
+next.addEventListener('click',() => {
+  currentActive++
+  console.log('currentActive CLICK', currentActive)
 })
