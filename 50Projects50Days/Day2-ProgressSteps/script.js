dc873243f1e4b51b3d4dff47d443b0ddbac5e7df
beerux <bastian_daniel@hotmail.com>
2024-03-30 10:10:31 +0100

    added js functionality

--- 50Projects50Days/Day2-ProgressSteps/script.js
+++ 50Projects50Days/Day2-ProgressSteps/script.js
@@ -1,5 +1,5 @@
 const progress = document.getElementById('progress')
-const previous = document.getElementById('previous')
+const previous = document.getElementById('prev')
 const next = document.getElementById('next')
 
 //Use "queryselectorAll" since there's more than 1
@@ -11,7 +11,34 @@ let currentActive = 1
 console.log(currentActive)
 
 //Listen for a click and add 1 to any element with "active" class
-next.addEventListener('click',() => {
+next.addEventListener('click', () => {
   currentActive++
-  console.log('currentActive CLICK', currentActive)
+
+  if (currentActive > circleNodes.length) {
+    currentActive = circleNodes.length
+  }
+
+    update()
+
+  console.log('currentActive', currentActive)
 })
+
+previous.addEventListener('click', () => {
+  currentActive--
+
+  if (currentActive < 1) {
+    currentActive = 1
+  }
+
+  update()
+})
+
+function update() {
+  circleNodes.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+}
